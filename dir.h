6d7ddeceff40f424a3087be9d08b9f60a318a7a3
Alex <olekgitara@wp.pl>
2015-10-17 18:46:17 +0200

    Added include guard

--- dir.h
+++ dir.h
@@ -1,5 +1,10 @@
+#ifndef _DIR_H_
+#define _DIR_H_
+
 /* interface.c functions */
 FILE *get_next_file(int fd, char *filename, const char *mode);
 
 /* core.c functions */
 int dir_get_next_filedes(int fd, char *filename);
+
+#endif
